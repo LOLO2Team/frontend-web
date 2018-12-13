ae17cb23badac690a4e8ea0c76c4495ef27ee85b
jacky.leung <jacky.leung@oocl.com>
2018-12-14 01:39:25 +0800

    [LEUNGJA] feat: manager assign order to parking boy

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Table, Divider, Tag } from 'antd';
 import { connect } from "react-redux";
+import { Layout, Form, Icon, Input, Button, } from 'antd';
 
 class OrderList extends Component {
   constructor(props) {
@@ -36,24 +37,31 @@ class OrderList extends Component {
       //   </span>
       // ),
     },
-    // {
-    //   title: 'Action',
-    //   key: 'action',
-    //   render: (text, record) => (
-    //     <span>
-    //       <a href="javascript:;" onClick={() => this.onClickEdit(
-    //         record.employeeId,
-    //         record.name,
-    //         record.username,
-    //         record.email,
-    //         record.phone,
-    //         record.rolesList,
-    //         record.status)}>Edit {record.name}</a>
-    //       <Divider type="vertical" />
-    //       <a href="javascript:;" onClick={() => this.onClickDelete(record.employeeId)}>Freeze</a>
-    //     </span>
-    //   )}
-    ];
+    {
+      title: 'Assign To',
+      key: 'assign',
+      render: (text, record) => (
+        <span>
+          {this.showAssignTo(record)}
+        </span>
+      )
+    }
+  ];
+
+  showAssignTo = record => {
+    if (this.props.myRole.includes("ROLE_MANAGER")) {
+      const refNum = record.orderId;
+      return <span>
+        <Input id={record.orderId} ref="assign" />
+        <Button className="margin-bottom-15" type="primary" onClick={() => this.assignOrderToEmployee(record, refNum)} >Assign</Button>
+      </span>;
+    }
+  }
+
+  assignOrderToEmployee = (order, refNum) => {
+    this.props.assignOrder(this.props.token, order, document.getElementById(refNum).value);
+    document.getElementById(refNum).value = '';
+  }
 
   componentWillMount() {
     this.props.getInitData(this.props.token);
@@ -65,7 +73,6 @@ class OrderList extends Component {
     clearInterval(this.interval);
   }
   render() {
-    console.log(this.props.orders)
     return (
       <div>
         <Table columns={this.columns} dataSource={this.props.orders} />
@@ -77,7 +84,8 @@ class OrderList extends Component {
 
 const mapStateToProps = state => ({
   orders: state.orders,
-  token: state.token
+  token: state.token,
+  myRole: state.myRole
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -91,12 +99,12 @@ const mapDispatchToProps = dispatch => ({
       mode: 'cors',
       method: 'GET'
     })
-    .then(res => res.json())
-    .then(res =>
-      dispatch({
-        type: "SET_PARKING_LOTS",
-        payload: res
-      }))
+      .then(res => res.json())
+      .then(res =>
+        dispatch({
+          type: "SET_PARKING_LOTS",
+          payload: res
+        }))
       .then(() =>
         fetch("https://parking-lot-backend.herokuapp.com/orders", {
           //getInitData: fetch("http://localhost:8081/orders", {
@@ -113,7 +121,31 @@ const mapDispatchToProps = dispatch => ({
           type: "SET_ORDERS",
           payload: res
         });
+      }),
+
+  assignOrder: (token, order, employeeId) =>
+    fetch("https://parking-lot-backend.herokuapp.com/orders/" + order.orderId + "/employeeId/" + employeeId, {
+      //fetch("http://localhost:8081/orders/" + order.orderId + "/employeeId/0",{
+      mode: 'cors',
+      method: 'PUT',
+      body: JSON.stringify({
+        "content": order.orderId,
+        "vehicleNumber": order.vehicleNumber,
+        "orderStatus": "parking",
+        "employeeId": employeeId
+      }),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': token
       })
+    })
+    .then(res => {
+      if (res.status === 200) {
+        alert("Order assigned to employee");
+      } else {
+        alert(res.status + " error occurred");
+      }
+    })
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(OrderList);
